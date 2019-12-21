4ecdf2ae8b34d2984bc4d1aba2498e73f0942ef3
Adil Rasiyani <=>
2019-12-20 18:43:26 -0600

    Updates

---
+++ precache-manifest.ed233e3de17f61565c1eb6893af77bb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11eb8de350a3c0c14908c50dabb41c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20838e5de35dab6d91eb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f51739ea4aca53411842",
+    "url": "/static/js/2.a9608c14.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a9608c14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20838e5de35dab6d91eb",
+    "url": "/static/js/main.1ec574e0.chunk.js"
+  },
+  {
+    "revision": "b6b18ffae501d7149ff3",
+    "url": "/static/js/runtime-main.b47a9b93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
